13a98b16f04d0d3b02c288cc9f70f93458e0deb1
patrick <patrickchow0803@gmail.com>
2020-04-05 21:58:12 -0400

    Added more details to be displayed on each meal item

--- meals/lib/widgets/meal_item.dart
+++ meals/lib/widgets/meal_item.dart
@@ -16,6 +16,40 @@ class MealItem extends StatelessWidget {
       @required this.affordability});
   void selectMeal() {}
 
+  // Used to convert enum Complexity value to a String
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.Simple:
+        return 'Simple';
+        break;
+      case Complexity.Challenging:
+        return 'Challenging';
+        break;
+      case Complexity.Hard:
+        return 'Hard';
+        break;
+      default:
+        return 'Unknown';
+    }
+  }
+
+  // Used to convert enum Affordability value to a String
+  String get affordablityText {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return 'Affordable';
+        break;
+      case Affordability.Pricey:
+        return 'Pricey';
+        break;
+      case Affordability.Luxurious:
+        return 'Luxurious';
+        break;
+      default:
+        return 'Unknown';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
@@ -26,8 +60,12 @@ class MealItem extends StatelessWidget {
         margin: EdgeInsets.all(10),
         child: Column(
           children: <Widget>[
+            // Use a stack for when you want to place widgets on top of each other.
+            // In this example, A Text Widget is being placed on top of a Image
+            // The first item in a stack if the bottom layer. The next item is the layer above that and so fourth.
             Stack(
               children: <Widget>[
+                // Use ClipRRect for when you want to force a widget's dimensions.
                 ClipRRect(
                   borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(15.0),
@@ -41,8 +79,58 @@ class MealItem extends StatelessWidget {
                     width: double.infinity,
                     fit: BoxFit.cover,
                   ),
+                ),
+                // Positioned Widget is only used within a stack for when you want to position a widget better
+                Positioned(
+                  // The child widget should be 20 pixels from the bottom and 10 rights from the right
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.7,
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                      // Text Widget must be inside of another widget with dimensions for these properties to work
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
                 )
               ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.schedule),
+                      SizedBox(width: 6),
+                      Text('$duration mins')
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.work),
+                      SizedBox(width: 6),
+                      Text(complexityText)
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.attach_money),
+                      SizedBox(width: 6),
+                      Text(affordablityText)
+                    ],
+                  )
+                ],
+              ),
             )
           ],
         ),
