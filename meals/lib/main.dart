0c8176f443f970a2326f0cf1d06673faedb465d1
patrick <patrickchow0803@gmail.com>
2020-04-06 20:25:04 -0400

    Added an onUnknownRoute in case there's a problem with any of the navigation

--- meals/lib/main.dart
+++ meals/lib/main.dart
@@ -34,6 +34,9 @@ class MyApp extends StatelessWidget {
           CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
           MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
         },
+        onUnknownRoute: (settings){
+          return MaterialPageRoute(builder: (ctx) => CategoryMealsScreen());
+        },
       ),
     );
   }
